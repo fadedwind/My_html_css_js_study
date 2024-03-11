0b488abdceb7fe8023e66d409a128b129f92f954
Fadedwind <elwind297@outlook.com>
2024-03-12 07:18:41 +0800

    html_css

---
+++ index.js
@@ -0,0 +1,10 @@
+console.log(`Hello`);
+console.log(`I love pizza!`);
+
+window.alert(`This is an alert!`);
+window.alert(`I love pizza!`);
+
+document.getElementById("myH1").textContent = 'Hello';
+document.getElementById("myp").textContent = `I lovw pizza~`;
+
+//comment
